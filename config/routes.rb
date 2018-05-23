4e2bd96ec7d7447ed223747b9c73cb58b68f99d5
David Zwagemaker <david@zwagemaker.nl>
2018-05-23 17:04:10 +0100

    Airbnb app with index and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'flats#index'
+
+  # get    'flats/new',            to: 'flats#new'        #C
+  # post   'flats/create',         to: 'flats#create'     #C
+
+  # get    'flats',                to: 'flats#index'      #R
+  # get    'flats/:id',            to: 'flats#show'       #R
+
+  # get    'flats/:id/edit',       to: 'flats#edit'       #U
+  # patch  'flats/:id/update',     to: 'flats#update'     #U
+
+  # delete 'flats/:id',            to: 'flats#destroy'    #D
+
+  resources :flats
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating flats"
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Amazing beachfront apartment',
+  address: '6 Rua dos Pescadores Costa da Caparica Portugal',
+  description: 'Right by the beach, this apartment offers luxury and a direct access to kilometers of beachfront. Lisbon is only 30 minutes away by car or public transport',
+  price_per_night: 200,
+  number_of_guests: 6
+)
+Flat.create!(
+  name: 'Spacious Mansion in Amsterdam outskirts',
+  address: 'Heuvellaan 13, 1217 JL, Hilversum, Netherlands',
+  description: 'This house, built in 1987 and renovated 10 years ago, offers all the space and comfort you can wish for. Take a refreshing dive in the pool after taking a late night sauna.',
+  price_per_night: 400,
+  number_of_guests: 12
+)
